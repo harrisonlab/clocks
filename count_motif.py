048285bda58809a95257f36ac7cb37c7993c3310
Richard Harrison <richard.harrison@emr.ac.uk>
2015-03-21 23:53:54 +0000

    Seems to be looping ok

--- count_motif.py
+++ count_motif.py
@@ -4,13 +4,11 @@ handle = open("vd.fasta", "rU")
 for record in SeqIO.parse(handle, "fasta") :
 	print record.id,len(record.seq)
 	stringseq= ''.join(record.seq)
-	#matchObj = re.search( r'ATG', stringseq)
-	#matchObj = re.search( r'[GC]TACA[CG]TA', stringseq)
-	#matchObj = re.findall( r'G[CGA]CAGCCA', stringseq)
-	#matchObj = re.search( r'G[AG]CGGGA', stringseq)
-	#matchObj = re.search( r'GC[AG]CTAAC', stringseq)
-	pattern=r'G[CGA]CAGCCA'
-	regex = re.compile(pattern, re.IGNORECASE)
+	pattern1=r'[GC]TACA[CG]TA'
+	pattern2=r'G[CGA]CAGCCA'
+	pattern3=r'G[AG]CGGGA'
+	pattern4=r'GC[AG]CTAAC'
+	regex = re.compile(pattern2, re.IGNORECASE)
 	for match in regex.finditer(stringseq):
 		print "%s: %s" % (match.start(), match.group())
 
