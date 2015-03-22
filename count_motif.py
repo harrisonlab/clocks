32389ca88f39ea332d762e09883e4b7d117b5f0d
Richard Harrison <richard.harrison@emr.ac.uk>
2015-03-22 08:54:48 +0000

    count motif working and outputting

--- count_motif.py
+++ count_motif.py
@@ -1,18 +1,16 @@
 import re
 from Bio import SeqIO
-handle = open("vd.fasta", "rU")
+handle = open("nc.fasta", "rU")
 for record in SeqIO.parse(handle, "fasta") :
-	print record.id,len(record.seq)
+	#print record.id,len(record.seq)
 	stringseq= ''.join(record.seq)
 	pattern1=r'[GC]TACA[CG]TA'
 	pattern2=r'G[CGA]CAGCCA'
 	pattern3=r'G[AG]CGGGA'
 	pattern4=r'GC[AG]CTAAC'
-	regex = re.compile(pattern2, re.IGNORECASE)
-	for match in regex.finditer(stringseq):
-		print "%s: %s" % (match.start(), match.group())
-
-	
-
-
+	patterns=(pattern1,pattern2,pattern3,pattern4)
+	for pat in patterns:
+		regex = re.compile(pat, re.IGNORECASE)
+		for match in regex.finditer(stringseq):
+			print "%s\t %s\t %s\t %s\t" % (record.id, match.start(),match.end(), match.group())
 handle.close()
