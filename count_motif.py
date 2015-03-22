ee0dc928eb2bfdb4c1843547bd46042c2c6b99ff
Richard Harrison <richard.harrison@emr.ac.uk>
2015-03-22 22:11:28 +0000

    Motif search

--- count_motif.py
+++ count_motif.py
@@ -8,9 +8,19 @@ for record in SeqIO.parse(handle, "fasta") :
 	pattern2=r'G[CGA]CAGCCA'
 	pattern3=r'G[AG]CGGGA'
 	pattern4=r'GC[AG]CTAAC'
-	patterns=(pattern1,pattern2,pattern3,pattern4)
-	for pat in patterns:
+	fpatterns=(pattern1,pattern2,pattern3,pattern4)
+	for pat in fpatterns:
 		regex = re.compile(pat, re.IGNORECASE)
 		for match in regex.finditer(stringseq):
-			print "%s\t %s\t %s\t %s\t" % (record.id, match.start(),match.end(), match.group())
+			print "%s\t %s\t %s\t %s\t %s\t %s\t" % (record.id, match.start(),match.end(), match.group(),'+',pat)
+	pattern5=r'TA[CG]TGTA[GC]'
+	pattern6=r'TGGCTG[TCG]C'
+	pattern7=r'TCCCG[CT]G'
+	pattern8=r'GTTAG[CT]GC'
+	rpatterns=(pattern5,pattern6,pattern7,pattern8)
+	for pat in rpatterns:
+		regex = re.compile(pat, re.IGNORECASE)
+		for match in regex.finditer(stringseq):
+			print "%s\t %s\t %s\t %s\t %s\t %s\t" % (record.id,match.start(),match.end(), match.group(),'-',pat)
+		
 handle.close()
--- feature_search.py
+++ feature_search.py
@@ -1,75 +1,58 @@
-#from BCBio import GFF
-#from Bio import SeqIO
-
-#in_seq_file = "nc.fasta"
-#in_seq_handle = open(in_seq_file)
-#seq_dict = SeqIO.to_dict(SeqIO.parse(in_seq_handle, "fasta"))
-#in_seq_handle.close()
-
-#in_file = "nc.gff3"
-#limit_info = dict(
-#        gff_id = ["7.23"],
-#        gff_type = ["CDS"])
-#in_handle = open(in_file)
-
-#for rec in GFF.parse(in_handle, limit_info=limit_info):
-#   print rec.features[0]
-#in_handle.close()
- 
-#to do- work out how to efficiently query gff for each motif location and ask whether it 
-#upstream of a coding sequence- making sure that the orientation is correct 
-
-
 import gffutils
 
-
-#all of these coming from prior script output
-scaf='7.23'
-motif="TEST"
-m_start=7892
-m_end=7899
-m_dir=-1
-#arbitrary upstream distance
-a_fwd=1000
-
-#load the database
-db = gffutils.FeatureDB('nc', keep_order=True)
-record={}
-
-if m_dir == 1:
-	if len(subregion)>0:
-		subregion=  list(db.region(region=(scaf, m_start,m_end+a_fwd),featuretype='CDS',completely_within=False)) 
-		record=subregion[0]
-	else:
-		record=0
-#NOTE WHEN THIS IS GOING THE 'OTHER' WAY SUBREGION[0] APPEARS TO SELECT THE FURTHEST AWAY
-#WHEN THERE ARE >1 HIT, NOT THE NEAREST SO IT NEEDS ALTERING
-else:
-	subregion=  list(db.region(region=(scaf, m_end-a_fwd,m_end),featuretype='CDS',completely_within=False))
-	if len(subregion)>0:
-		sub_val=len(subregion)-1
-		record=subregion[sub_val]
-	else:
-		record=0
- 
-if record ==0:
-	print "No hit"
-else:
-#SELECT ONLY FIRST HIT IMMEDIATELY DOWNSTREAM
-#for record in subregion :
-	if record.strand == '+' and m_dir == 1:
-		print "Motif",motif
-		print "Motif start",m_start
-		upstream=record.start-m_start
-		print 'Motif upstream', upstream, "bp"
-		print record['ID'],record.start,record.end,record.strand	
-	elif record.strand == '-' and m_dir == -1:
-		print "Motif",motif
-		print "Motif start",m_start
-		print "Motif dir",m_dir
-		upstream=(record.end-m_end)*-1
-		print 'Motif upstream', upstream, "bp"
-		print record['ID'],record.start,record.end,record.strand
-	else:
-		print "Strands incorrect"
+f = open("nc_motifs.txt",'r')
+for line in f.readlines():
+	values=line.split('\t')
+	scaf=values[0]
+	m_start=int(values[1])
+	m_end=int(values[2])
+	motif=values[3]
+	m_dir=values[4].strip()
+	#print values[0],values[1],values[2],values[3],values[4]
+	#print scaf,m_start,m_end,motif,m_dir
+	a_fwd=1000
+	#load the database
+	db = gffutils.FeatureDB('nc', keep_order=True)
+	record={}
+	subregion={}
 	
+	if m_dir == '+':
+		#print "Here +"
+		subregion=  list(db.region(region=(scaf, m_start,m_end+a_fwd),featuretype='CDS',completely_within=False)) 		
+		if len(subregion)>0:
+			record=subregion[0]
+		else:
+			record=0
+	elif m_dir == '-':
+		#print "Here -"
+		subregion=  list(db.region(region=(scaf, m_end-a_fwd,m_end),featuretype='CDS',completely_within=False))
+		if len(subregion)>0:
+			sub_val=len(subregion)-1
+			record=subregion[sub_val]
+		else:
+			record=0
+	else:
+		print "Error"
+		break
+		
+	if record ==0:
+		#print "No hit"
+		next
+	else:
+	#SELECT ONLY FIRST HIT IMMEDIATELY DOWNSTREAM
+		if record.strand == '+' and m_dir == '+':
+			upstream=record.start-m_end
+			if upstream<0:
+				next
+			else:
+				print motif,m_start,upstream,record['ID'],record.start,record.end,record.strand
+		elif record.strand == '-' and m_dir == '-':
+			upstream=(record.end-m_end)*-1
+			if upstream<0:
+				next
+			else:
+				print motif,m_start,upstream,record['ID'],record.start,record.end,record.strand
+		else:
+			#print "Strands incorrect"
+			next
+f.close()
